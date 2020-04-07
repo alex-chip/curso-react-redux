35b3c4765140f9fe8470eb0cfdacf2aaa5b26e08
Alexander Chip <alexchip32@gmail.com>
2020-04-06 19:32:40 -0600

    Agregando redux a la aplicacion

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const NotFound = () => (
-  <>
+  <React.Fragment>
     <h1>Not Found</h1>
-  </>
-)
+  </React.Fragment>
+);
 
-export default NotFound
+export default NotFound;
