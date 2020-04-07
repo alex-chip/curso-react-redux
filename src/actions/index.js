461d4b44260116a1055de8c92623ad3133da09da
Alexander Chip <alexchip32@gmail.com>
2020-04-06 21:12:48 -0600

    Agregando funcionalidad de favoritos a la app

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const setFavorite = payload => ({
+  type: 'SET_FAVORITE',
+  payload,
+});
+
+export const deleteFavorite = payload => ({
+  type: 'DELETE_FAVORITE',
+  payload,
+});
+
+// export {
+//   setFavorite,
+//   deleteFavorite,
+// };
